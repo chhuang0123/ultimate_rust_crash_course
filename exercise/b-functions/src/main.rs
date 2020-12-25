38ad6593aadc1cbd3a97329fa45e113969c811f3
chhuang <chhuang@gmail.com>
2020-12-26 01:36:59 +0800

    remove bracket

--- exercise/b-functions/src/main.rs
+++ exercise/b-functions/src/main.rs
@@ -2,11 +2,9 @@ fn main() {
     let width = 4;
     let height = 7;
     let depth = 10;
-    {
-        let area = area_of(width, height);
-        println!("Area is {}", area);
-    }
 
+    let area = area_of(width, height);
+    println!("Area is {}", area);
     println!("Volume is {}", volume(width, height, depth));
 }
 
